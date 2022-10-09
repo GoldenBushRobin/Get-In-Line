6fe1ceb2805bab135cfee16f2b65d3e6b78f5963
GoldenBushRobin <81373782+GoldenBushRobin@users.noreply.github.com>
2022-10-09 09:53:06 -0500

    Add files via upload

--- ocr.py
+++ ocr.py
@@ -1,10 +1,17 @@
 from tesserocr import PyTessBaseAPI
 
-images = ['training-strips/cartoon3.png']
-
+images = []
+for i in range(1,22):
+    prefix = "training-strips/cartoon"
+    images.append(f'{prefix}{i}.png')
+index = 1
 with PyTessBaseAPI(path = 'C:\\Users\\achau\\Desktop\\datathon\\getinline\\tessdata_best-main') as api:
     for img in images:
         api.SetImageFile(img)
         out = api.GetUTF8Text()
-print(out.upper())
+
+        file = open(f"outputs/tessenocr-out{index}.txt","w")
+        index += 1
+        file.write(out.upper())
+        file.close
 
